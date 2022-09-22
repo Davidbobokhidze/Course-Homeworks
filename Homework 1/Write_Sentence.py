2331fdcec28720b5bdff7fb55fa917b5fca3ad84
davidbobokhidze <thedavidbobo@gmail.com>
2022-09-22 14:21:13 +0400

    first commit

---
+++ Homework 1/Write_Sentence.py
@@ -0,0 +1,31 @@
+Name = "Davit"
+Surname = "Bobokhidze"
+Age = 24
+Origin = "Tbilisidan"
+Self = "Me var"
+Occupation = "programirebis studenti Goal oriented academy-shi"
+Selfage = "wlis"
+Teacher = "Nika Keshelava"
+Selfteacher = "chemi maswavlebeli aris"
+Height = 182.1
+Heightdefinition = "santimetri var simagleshi"
+
+print(Self + " " + Name + " " + Surname + " " + str(Age) + " " + Selfage + " "
+ + Origin + "," + " " + str(Height) + " " + Heightdefinition + "." + " " + Self + " " + 
+ Occupation + "," + " " + Selfteacher + " " + Teacher )
+
+NameENG = "Davit"
+SurnameENG = "Bobokhidze"
+AgeENG = 24
+OriginENG = "from Tbilisi, Georgia"
+SelfENG = "I'm"
+OccupationENG = "programming student at Goal oriented academy"
+SelfageENG = "years old"
+TeacherENG = "Nika Keshelava"
+SelfteacherENG = "My teacher is"
+HeightENG = 182.1
+HeightdefinitionENG = "my height in CM is"
+
+print(SelfENG + " " + NameENG + " " + SurnameENG + " " + str(AgeENG) + " " + SelfageENG + " "
+ + OriginENG + "." + " " + " " + HeightdefinitionENG + " " + str(HeightENG) + "," + " " + SelfENG + " " + 
+ OccupationENG + "." + " " + SelfteacherENG + " " + TeacherENG + "." )
